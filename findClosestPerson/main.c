ab021fb6435e10f3607b16b469fd955de819bd9f
Wallace Santana <wallacesantanak0@gmail.com>
2025-09-04 14:53:14 -0300

    feat(findClosestPerson)

---
+++ findClosestPerson/main.c
@@ -0,0 +1,12 @@
+int findClosest(int x, int y, int z) {
+    int dist1 = abs(z - x);
+    int dist2 = abs(z - y);
+    
+    if (dist1 < dist2) {
+        return 1;
+    } else if (dist2 < dist1) {
+        return 2;
+    } else {
+        return 0;
+    }
+}
